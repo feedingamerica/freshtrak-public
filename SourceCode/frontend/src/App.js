8036d7a7e00e0364f916a7e0564d53f0ed2e9f48
Basiltj07 <basiltj07@gmail.com>
2020-04-03 15:07:57 +0530

    Basic react structure updated

---
+++ SourceCode/frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss';
+import DashboardComponent from "./Modules/Dashboard/DashboardComponent";
+
+
+const App=() =>{
+  return (
+    <div className="App">
+      <header className="App-header">
+            <DashboardComponent/>
+      </header>
+    </div>
+  );
+};
+
+export default App;
---
+++ SourceCode/frontend/src/Modules/General/HeaderComponent.js
@@ -0,0 +1,16 @@
+/**
+ * Header Component
+ */
+
+import React from 'react';
+const HeaderComponent = () => { 
+    return (
+        <header className="header">
+            <div className="container">
+                Header Data
+            </div>
+        </header>
+    )
+};
+
+export default HeaderComponent;
