8b64870965f88488d8b21104076d38ec400f6e06
Ashik Varma <varmaashik@gmail.com>
2020-04-06 14:29:17 +0530

    Modified:
    * Changed Route.js to resolve merge conflict

--- SourceCode/frontend/src/Core/Routes.js
+++ SourceCode/frontend/src/Core/Routes.js
@@ -3,7 +3,7 @@ import React, {lazy, Suspense} from 'react';
 import '../Assets/Styles/Common/style.css';
 import {RENDER_URL} from '../Utils/Urls';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-const DashboardComponent = lazy(() => import("../Modules/Dashboard/DashboardComponent"));
+const DashBoardContainer = lazy(() => import("../Modules/Dashboard/DashBoardContainer"));
 /*const EventContainer = lazy(() => import("../Modules/Events/EventContainer"));
 const EventListContainer = lazy(() => import("../Modules/Events/EventListContaner"));*/
 const EventListComponent = lazy(() => import("../Modules/EventList/EventListComponent")); 
@@ -15,8 +15,7 @@ const Routes = () => {
             <Suspense fallback={<div className="displayNone"> </div>}>
                 <Switch>
                     <Route exact path={RENDER_URL.HOME_URL} component={App}/>
-                    
-                                        
+                    <Route path={RENDER_URL.EVENT_LIST_URL} component={EventListComponent}/>                    
                     
                 </Switch>
             </Suspense>
