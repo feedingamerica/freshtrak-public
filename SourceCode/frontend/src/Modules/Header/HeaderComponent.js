cc5cc39b467ae255fbc52c0efc3ce988fa9aa39b
Basiltj07 <basiltj07@gmail.com>
2020-04-08 18:12:59 +0530

    Header component rewriting

--- SourceCode/frontend/src/Modules/Header/HeaderComponent.js
+++ SourceCode/frontend/src/Modules/Header/HeaderComponent.js
@@ -7,6 +7,8 @@ import React, { useEffect, useState } from 'react';
 import mainLogo from '../../Assets/img/logo.png';
 import navBarIcon from '../../Assets/img/menu.svg';
 import userIcon from '../../Assets/img/Mask.svg';
+import {Link} from "react-router-dom";
+import {Nav,NavDropdown,Navbar,DropdownItem} from 'react-bootstrap';
 const HeaderComponent = () => {
     const [navbarShrink, setNavbarShrink] = useState('');
     useEffect(() => {
@@ -19,7 +21,7 @@ const HeaderComponent = () => {
         }
     }, []);;
     return (
-    	 <nav className={`navbar navbar-expand-md navbar-light fixed-top ${navbarShrink}`} id="mainNav">
+    	/*<nav className={`navbar navbar-expand-md navbar-light fixed-top ${navbarShrink}`} id="mainNav">
                 <div className="container">
                     <div>
                         <h3 className="my-auto mobile-view">
@@ -69,7 +71,54 @@ const HeaderComponent = () => {
                        
                     </div>
                 </div>
-            </nav>
+            </nav>*/
+            <Nav className={`navbar navbar-expand-md navbar-light fixed-top ${navbarShrink}`} id="mainNav">
+                <div className="container">
+                    <Navbar expand="md" >
+                        <Navbar.Brand href="#home">                            
+                                <h3 className="my-auto mobile-view">                                    
+                                    <img src={mainLogo} alt="FreshTrak" className="d-inline-block" />                                    
+                                </h3>
+                                <button className="navbar-toggler mr-2" type="button" data-toggle="collapse"
+                                    data-target="#navbarCollapse">
+                                    <span className="navbar-toggler-icon">
+                                        <img src={navBarIcon} alt="UserLogo" className="img-fluid"/>
+                                    </span>
+                                </button>
+                            
+                        </Navbar.Brand>        
+                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                            <Navbar.Collapse id="navbarCollapse" className="justify-content-end">
+                                <Nav className="navbar-nav ml-auto small mt-3 mt-md-0 mb-2 mb-md-0 main-menu align-items-center">
+                                    <NavDropdown title="Find Resources" aria-labelledby="dropdown01">                                   
+                                        <NavDropdown.Item className="dropdown-item" href="#1">Action</NavDropdown.Item>
+                                        <NavDropdown.Item className="dropdown-item" href="#2">Another action</NavDropdown.Item>
+                                        <NavDropdown.Item className="dropdown-item" href="#3">Something else here</NavDropdown.Item>
+                                    </NavDropdown>
+                                </Nav>
+                                <Nav className="navbar-nav ml-auto small mt-3 mt-md-0 mb-2 mb-md-0 main-menu align-items-center">
+                                    <NavDropdown title="For Food Banks" aria-labelledby="dropdown01">                                   
+                                        <NavDropdown.Item className="dropdown-item" href="#4">Action</NavDropdown.Item>
+                                        <NavDropdown.Item className="dropdown-item" href="#6">Another action</NavDropdown.Item>
+                                        <NavDropdown.Item className="dropdown-item" href="#7">Something else here</NavDropdown.Item>
+                                    </NavDropdown>
+                                </Nav>
+                            </Navbar.Collapse>                        
+                            <div className="user-avatar">
+                                <NavDropdown eventKey={1} title={                                    
+                                    <img className="thumbnail-image" src={userIcon} alt="user pic" />
+                                     }> 
+                                    <DropdownItem eventKey={1.1} href="/profile">Basil TJ</DropdownItem> 
+                                    <DropdownItem eventKey={1.1} href="/profile">Profile</DropdownItem> 
+                                    <DropdownItem divider /> 
+                                    <DropdownItem eventKey={1.3}> 
+                                        <i className="fa fa-sign-out"></i> Logout 
+                                    </DropdownItem> 
+                                </NavDropdown>
+                            </div>
+                    </Navbar>
+                </div>
+            </Nav>
     )
 };
 
