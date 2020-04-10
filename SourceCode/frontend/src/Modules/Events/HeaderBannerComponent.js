54b77124ff30ba6f074299cedc7bcc5d00e76c0f
Sean Lawrenz <sean.lawrenz@mbp-77.lan>
2020-04-10 14:13:46 -0400

    Removed invalid JSX class

--- SourceCode/frontend/src/Modules/Events/HeaderBannerComponent.js
+++ SourceCode/frontend/src/Modules/Events/HeaderBannerComponent.js
@@ -3,9 +3,9 @@ import '../../Assets/scss/main.scss';
 const HeaderBannerDataComponent = () => {
     return (
         <div className="container h-100">
-            <div class="header-content h-100 d-flex flex-column align-items-center justify-content-center">
-                <div class="banner-content">
-                    <h1 class="text-center">
+            <div className="header-content h-100 d-flex flex-column align-items-center justify-content-center">
+                <div className="banner-content">
+                    <h1 className="text-center">
                         Food resources near you
                     </h1>
                 </div>
