c2a79ed0cccc3592b5f8997acd919cc5cc5b02df
Sean Lawrenz <seanlawrenz@megans-mbp.lan>
2020-04-11 20:05:58 -0400

    event handler for parseing event data

--- SourceCode/frontend/src/Testing/mock-foodbanks/index.js
+++ SourceCode/frontend/src/Testing/mock-foodbanks/index.js
@@ -13,26 +13,4 @@ const mockFoodBankBuilder = build('Food Bank').fields({
   fb_fano_url: fake(f => f.internet.url()),
 });
 
-const mockFoodBankEventAddressBuilder = build('Food Bank Event').fields({
-  id: fake(f => f.random.number()),
-  address: fake(f => f.address.streetAddress()),
-  city: fake(f => f.address.city()),
-  state: fake(f => f.address.state()),
-  zip: fake(f => f.address.zipCode()),
-  phone: fake(f => f.phone.phoneNumber()),
-  name: fake(f => f.random.word()),
-  nickname: fake(f => f.random.word()),
-  event_dates: []
-});
-
-const mockEventBuilder = build('Event Dates').fields({
-  id: fake(f => f.random.number()),
-  service: fake(f => f.random.word()),
-  start_time: fake(f => f.date.future()),
-  end_time: fake(f => f.date.future()),
-  date: fake(f => f.date.future()),
-});
-
 export const mockFoodBank = mockFoodBankBuilder();
-export const mockFoodBankEventAddress = mockFoodBankEventAddressBuilder();
-export const mockEvent = mockEventBuilder();
