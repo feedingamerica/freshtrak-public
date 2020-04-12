8f074e762ef12649b8b5d0cdc18633e886238b9b
nvengal <nikhil@boldpenguin.com>
2020-04-12 00:34:58 -0400

    Add react routing fixes

--- client/src/Core/Routes.js
+++ client/src/Core/Routes.js
@@ -4,7 +4,7 @@ import React, {lazy, Suspense} from 'react';
 import {RENDER_URL} from '../Utils/Urls';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 const AuthenticatedLandingComponent =  lazy(() => import('../Modules/AuthenticatedLanding/AuthenticatedLandingComponent'));
-const ConfirmationComponent = lazy(() => import("../Modules/Confirmation/ConfirmationComponent")); 
+const ConfirmationComponent = lazy(() => import("../Modules/Confirmation/ConfirmationComponent"));
 const DashBoardContainer = lazy(() => import("../Modules/Dashboard/DashBoardContainer"));
 /*const EventContainer = lazy(() => import("../Modules/Events/EventContainer"));
 const EventListContainer = lazy(() => import("../Modules/Events/EventListContaner"));*/
@@ -14,12 +14,12 @@ const App = lazy(() => import("../App"));
 
 const Routes = () => {
     return (
-        <Router>
+        <Router basename="/">
             <Suspense fallback={<div className="displayNone"> </div>}>
                 <Switch>
-                    <Route path={RENDER_URL.HOME_URL} component={App}/>
+                    <Route exact path={RENDER_URL.HOME_URL} component={App}/>
                     <Route path={RENDER_URL.EVENT_CONFIRM_URL} component={ConfirmationComponent}/>
-                    <Route path={RENDER_URL.EVENT_LIST_URL} component={EventListComponent}/>   
+                    <Route path={RENDER_URL.EVENT_LIST_URL} component={EventListComponent}/>
                     <Route path={RENDER_URL.DASHBOARD_URL} component={DashBoardContainer}/>
                     <Route path={RENDER_URL.ADD_FAMILY_URL} component={FamilyContainer}/>
                     <Route path={RENDER_URL.AUTHENTICATED_USER_URL} component={AuthenticatedLandingComponent}/>
