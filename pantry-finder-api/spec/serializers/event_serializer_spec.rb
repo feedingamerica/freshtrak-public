4dd70fc460fe9a276c3c945ca136faecf3c87c29
Phil Norman <philnorman2@gmail.com>
2020-04-11 21:40:58 -0400

    pushing failed test event_serializer_spec.rb for collaboration

--- pantry-finder-api/spec/serializers/event_serializer_spec.rb
+++ pantry-finder-api/spec/serializers/event_serializer_spec.rb
@@ -2,11 +2,19 @@
 
 describe EventSerializer do
   it 'JSON Event Display fields' do
-    described_class.new(
-      Event.new(event_name: 'big event', address1: 'addr 1',
-                city: 'the town', state: 'OH', zip: '12345',
-                pt_latitude: '40.1479133',
-                pt_longitude: '-83.0959877', loc_id: '6')
-    )
+    serializer =
+      described_class.new(
+        Event.new(event_name: 'big event', address1: 'addr 1',
+                  city: 'the town', state: 'OH', zip: '12345',
+                  pt_latitude: '40.1479133',
+                  pt_longitude: '-83.0959877', loc_id: '6')
+      )
+
+    exp_rslt = '{"name":"big event". ""address":"addr 1 addr 2",' \
+                 '"city":"the town","state":"OH",' \
+                 '"zip":"12345","pt_latitude":"40.1479133",' \
+                 '"pt_longitude":"-83.0959877", "loc_id":"6"}'
+
+    expect(serializer.to_json).to eql(exp_rslt)
   end
 end
