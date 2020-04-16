325ab0887605a42753a68fd963507202d3ad44d2
Sean Lawrenz <sean.lawrenz@mbp-77.lan>
2020-04-16 13:22:29 -0400

    Added formatting to address of events Removed staic phone number

--- client/src/Modules/Events/ResourceListComponent.js
+++ client/src/Modules/Events/ResourceListComponent.js
@@ -18,19 +18,21 @@ const ResourceListComponent = (props) => {
         <div className="search-results">
             <div className="search-list-title">Your Local Food Banks</div>
             {foodBankArray.map((value, index) => {
+                const { foodbank: { name, address, city, state, zip, phone, fb_url, fb_fano_url } } = value;
                 return(
                     <div className="row align-items-center mt-2" key={index}>
                         <div className="col-lg-4 col-sm-6">
                             <div className="d-flex align-items-center">
                                 <span className="search-list-logo"><img src={mofcLogo}/></span>
-                                <span className="font-weight-bold ml-2">{value.foodbank.name}</span>
+                                <span className="font-weight-bold ml-2">{name}</span>
                             </div>
                         </div>
-                        <div className="col-lg-4 col-sm-6 caption-text">{value.foodbank.address},
-                            {value.foodbank.city}, {value.foodbank.state} {value.foodbank.zip}</div>
                         <div className="col-lg-4 col-sm-6 caption-text">
-                            <div>(614) 274 7770</div>
-                            <div>{value.foodbank.fb_url}</div>
+                            {address} {city}, {state} {zip}
+                        </div>
+                        <div className="col-lg-4 col-sm-6 caption-text">
+                            <div>{phone}</div>
+                            <div>{fb_url}</div>
                         </div>
                     </div>
                 )
