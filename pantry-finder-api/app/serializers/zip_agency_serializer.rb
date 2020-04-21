57c4f50899f05454b83e17a07d78e14e03411490
Phil Norman <philnorman2@gmail.com>
2020-04-21 11:11:57 -0400

    removing zip_agency_serializer.rb

--- pantry-finder-api/app/serializers/zip_agency_serializer.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Serializer to ensure agencies are children of ZipAgencies
-class ZipAgencySerializer < ActiveModel::Serializer
-  attributes :id
-
-  has_many :agencies
-end
