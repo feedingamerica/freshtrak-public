a64c35244bbe123f5c3bf17da50c0daa09c3fb9a
nvengal <nikhil@boldpenguin.com>
2020-04-22 10:54:31 -0400

    Add google analytics
    Refactor build configuration

--- client/src/Utils/Urls.js
+++ client/src/Utils/Urls.js
@@ -2,23 +2,7 @@
  * API and Render url defined
  */
 
-const getBaseUrl = () => {
-    let env = process.env.REACT_APP_STAGE;
-    switch (env) {
-        case 'dev':
-            return 'http://localhost:8888/';
-        case 'beta':
-            return 'https://pantry-finder-api.beta.freshtrak.com/';
-        case 'staging':
-            return 'http://api-freshtrak.inapp.com/';
-        case 'production':
-            return 'http://api-freshtrak.inapp.com/';
-        default:
-            return 'http://localhost:8888/';
-    }
-};
-
-export const BASE_URL = getBaseUrl();
+export const BASE_URL = process.env.REACT_APP_PANTRY_FINDER_API
 export const API_URL = {
     EVENTS_LIST : BASE_URL + 'api/agencies',
     FOODBANK_LIST : BASE_URL + 'api/foodbanks',
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 
-
+const GA_ID = process.env.REACT_APP_GA_ID;
+if (GA_ID) {
+  ReactGA.initialize(GA_ID);
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
 
 ReactDOM.render(<App/>,document.getElementById('root'));
-
